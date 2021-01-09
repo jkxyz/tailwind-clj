b1489301962825de8b67d238fa35ece65bc74753
Josh Kingsley <josh@joshkingsley.me>
2021-01-09 21:41:48 +0200

    Expand example

---
+++ example/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["target/prod/tailwind-used-classes"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
